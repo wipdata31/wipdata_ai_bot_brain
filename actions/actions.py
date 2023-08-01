3d7321c19467935bbac381def7fde1ec5c5b4509
chowdhuryshakur <chowdhuryshakur@gmail.com>
2023-08-01 06:21:51 +0600

    latest14

--- actions/actions.py
+++ actions/actions.py
@@ -135,30 +135,6 @@ class UpdateEmailSlotAction(Action):
         value = tracker.latest_message.get("text").lower().replace("emaill ", "").replace(" my email is ", "").replace(" patient's email is ", "").replace(" email ", "").replace(" patients email is ", "")
         return [SlotSet('email', value)]
 
-def validate_ic_with_dob(ic_number, date_of_birth):
-    # Ensure ic_number and date_of_birth are strings
-    ic_number = str(ic_number)
-    date_of_birth = str(date_of_birth)
-
-    # Check if ic_number and date_of_birth have the correct lengths
-    if len(ic_number) != 12:
-        return False
-
-    # Extract DD, MM, and YY from the IC number
-    dd = ic_number[:2]
-    mm = ic_number[2:4]
-    yy = ic_number[4:6]
-
-    # Construct the birthdate in the format dd/mm/yyyy
-    birthdate_from_ic = f"{dd}/{mm}/{yy}"
-    check_date = datetime.strptime(date_of_birth, "%d/%m/%Y").strftime("%d/%m/%y")
-    # Compare the IC's birthdate with the provided date_of_birth
-    if birthdate_from_ic != check_date:
-        return False
-
-    # IC number and date of birth match, return True
-    return True
-
 class UpdateICNumSlotAction(Action):
     def name(self) -> Text:
         return "action_tell_icnum"
@@ -170,15 +146,6 @@ class UpdateICNumSlotAction(Action):
         nationality = tracker.slots.get("nationality")
         dob = tracker.slots.get("dob")
         return [SlotSet('icnum', value)]
-        # if nationality == 'malaysian':
-        #     if validate_ic_with_dob(value.replace('-', '').replace(' ', ''), dob):
-        #         return [SlotSet('icnum', value)]
-        #     else:
-        #         print("incorrect IC.")
-        #         dispatcher.utter_message(text="Sorry, the IC number is incorrect. Please provide the valid IC number.")
-        #         return [SlotSet('icnum', None)] 
-        # else:
-        #     return [SlotSet('icnum', value)]
 
 class UpdateICNumSlot1Action(Action):
     def name(self) -> Text:
@@ -191,24 +158,6 @@ class UpdateICNumSlot1Action(Action):
         dob = tracker.slots.get("dob")
         value = tracker.latest_message.get("text").replace("appoiic ", "").replace(" ic number is ", "").replace(" ic number ", "")
         return [SlotSet('icnum', value)]
-        # if nationality == 'malaysian':
-        #     if validate_ic_with_dob(value.replace('-', '').replace(' ', ''), dob):
-        #         return [SlotSet('icnum', value)]
-        #     else:
-        #         print("incorrect IC.")
-        #         dispatcher.utter_message(text="Sorry, the IC number is incorrect. Please provide the valid IC number.")
-        #         return [SlotSet('icnum', None)] 
-        # else:
-        #     return [SlotSet('icnum', value)]
-
-def validate_date_format(date_str):
-    try:
-        # Attempt to parse the date string using the specified format
-        datetime.strptime(date_str, '%d/%m/%Y')
-        return True
-    except ValueError:
-        # If the date string is not in the correct format, ValueError will be raised
-        return False
 
 class UpdateDOBSlotAction(Action):
 
@@ -220,14 +169,19 @@ class UpdateDOBSlotAction(Action):
     ) -> List[Text]:
         value = tracker.latest_message.get("text").lower().replace("dateofbirth ", "").replace(" date of birth is ", "").replace(" patient's date of birth is ", "").replace(" patient date of birth is ", "")
 
-        if validate_date_format(value):
-            # The date format is valid
-            return [SlotSet('dob', value)]
-        else:
-            print("incorrect date.")
-            # The date format is invalid
-            dispatcher.utter_message(text="Sorry, the date format is incorrect. Please provide the date in the format DD/MM/YYYY.")
-            return [SlotSet('dob', None)] 
+        return [SlotSet('dob', value)]
+
+class UpdateDOBSlotActionTwo(Action):
+
+    def name(self) -> Text:
+        return "action_tell_dob_two"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[Text]:
+        value = tracker.latest_message.get("text").lower().replace("dobtwo ", "").replace(" date of birth is ", "").replace(" patient's date of birth is ", "").replace(" patient date of birth is ", "")
+
+        return [SlotSet('dob', value)]
 
 class UpdateFullNameSlotAction(Action):
     def name(self) -> Text:
